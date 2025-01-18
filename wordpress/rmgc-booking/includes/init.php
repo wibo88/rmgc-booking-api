7e061c15e4a71b8bfae35a42f3cf0abaf822edc1
wibo88 <wfwatt@gmail.com>
2025-01-19 10:50:12 +1100

    refactor: Remove database functions from init.php

--- wordpress/rmgc-booking/includes/init.php
+++ wordpress/rmgc-booking/includes/init.php
@@ -1,30 +1,4 @@
 <?php
-// Create the bookings table on plugin activation
-function rmgc_create_bookings_table() {
-    global $wpdb;
-    $table_name = $wpdb->prefix . 'rmgc_bookings';
-    
-    $charset_collate = $wpdb->get_charset_collate();
-
-    $sql = "CREATE TABLE IF NOT EXISTS $table_name (
-        id mediumint(9) NOT NULL AUTO_INCREMENT,
-        booking_date date NOT NULL,
-        players int(11) NOT NULL,
-        handicap int(11) NOT NULL,
-        email varchar(100) NOT NULL,
-        status varchar(20) NOT NULL DEFAULT 'pending',
-        created_at datetime DEFAULT CURRENT_TIMESTAMP,
-        PRIMARY KEY  (id)
-    ) $charset_collate;";
-
-    require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
-    dbDelta($sql);
-    
-    // Add default notification email
-    add_option('rmgc_notification_emails', get_option('admin_email'));
-}
-register_activation_hook(__FILE__, 'rmgc_create_bookings_table');
-
 // Enqueue scripts and styles
 function rmgc_enqueue_scripts() {
     wp_enqueue_script('jquery');
@@ -35,12 +9,4 @@ function rmgc_enqueue_scripts() {
     wp_enqueue_script('google-recaptcha', 'https://www.google.com/recaptcha/api.js');
     
     wp_enqueue_script('rmgc-booking', plugin_dir_url(dirname(__FILE__)) . 'js/booking.js', array('jquery', 'jquery-ui-datepicker'), time(), true);
-    
-    wp_localize_script('rmgc-booking', 'rmgcApi', array(
-        'apiUrl' => get_option('rmgc_api_url'),
-        'apiKey' => get_option('rmgc_api_key'),
-        'siteUrl' => get_site_url(),
-        'recaptchaSiteKey' => get_option('rmgc_recaptcha_site_key')
-    ));
 }
-add_action('wp_enqueue_scripts', 'rmgc_enqueue_scripts');
