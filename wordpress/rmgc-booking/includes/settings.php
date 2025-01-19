6d552cbafc4d45fabfd127608d14a9b992c4b7fc
wibo88 <wfwatt@gmail.com>
2025-01-19 21:07:34 +1100

    feat: Add email configuration options to settings page

--- wordpress/rmgc-booking/includes/settings.php
+++ wordpress/rmgc-booking/includes/settings.php
@@ -5,7 +5,9 @@ function rmgc_register_settings() {
     register_setting('rmgc_settings', 'rmgc_api_key');
     register_setting('rmgc_settings', 'rmgc_recaptcha_site_key');
     register_setting('rmgc_settings', 'rmgc_recaptcha_secret_key');
-    register_setting('rmgc_settings', 'rmgc_notification_email');
+    register_setting('rmgc_settings', 'rmgc_admin_notification_emails');
+    register_setting('rmgc_settings', 'rmgc_admin_email_subject');
+    register_setting('rmgc_settings', 'rmgc_guest_email_subject');
     register_setting('rmgc_settings', 'rmgc_email_from_name');
     register_setting('rmgc_settings', 'rmgc_email_from_address');
 }
@@ -17,7 +19,9 @@ function rmgc_booking_settings_page() {
         return;
     }
 
-    $notification_email = get_option('rmgc_notification_email', get_option('admin_email'));
+    $admin_notification_emails = get_option('rmgc_admin_notification_emails', get_option('admin_email'));
+    $admin_email_subject = get_option('rmgc_admin_email_subject', 'New Booking Request Received - RMGC');
+    $guest_email_subject = get_option('rmgc_guest_email_subject', 'Your Booking Request - Royal Melbourne Golf Club');
     $email_from_name = get_option('rmgc_email_from_name', get_bloginfo('name'));
     $email_from_address = get_option('rmgc_email_from_address', get_option('admin_email'));
 
@@ -30,7 +34,14 @@ function rmgc_booking_settings_page() {
         update_option('rmgc_api_key', sanitize_text_field($_POST['rmgc_api_key']));
         update_option('rmgc_recaptcha_site_key', sanitize_text_field($_POST['rmgc_recaptcha_site_key']));
         update_option('rmgc_recaptcha_secret_key', sanitize_text_field($_POST['rmgc_recaptcha_secret_key']));
-        update_option('rmgc_notification_email', sanitize_email($_POST['rmgc_notification_email']));
+        
+        // Sanitize admin notification emails (allow multiple, comma-separated)
+        $admin_emails = array_map('trim', explode(',', sanitize_text_field($_POST['rmgc_admin_notification_emails'])));
+        $admin_emails = array_filter($admin_emails, 'is_email');
+        update_option('rmgc_admin_notification_emails', implode(',', $admin_emails));
+        
+        update_option('rmgc_admin_email_subject', sanitize_text_field($_POST['rmgc_admin_email_subject']));
+        update_option('rmgc_guest_email_subject', sanitize_text_field($_POST['rmgc_guest_email_subject']));
         update_option('rmgc_email_from_name', sanitize_text_field($_POST['rmgc_email_from_name']));
         update_option('rmgc_email_from_address', sanitize_email($_POST['rmgc_email_from_address']));
 
@@ -69,11 +80,31 @@ function rmgc_booking_settings_page() {
             <table class="form-table">
                 <tr>
                     <th scope="row">
-                        <label for="rmgc_notification_email">Notification Email</label>
+                        <label for="rmgc_admin_notification_emails">Admin Notification Emails</label>
                     </th>
                     <td>
-                        <input type="email" id="rmgc_notification_email" name="rmgc_notification_email" value="<?php echo esc_attr($notification_email); ?>" class="regular-text">
-                        <p class="description">Email address to receive booking notifications</p>
+                        <input type="text" id="rmgc_admin_notification_emails" name="rmgc_admin_notification_emails" value="<?php echo esc_attr($admin_notification_emails); ?>" class="regular-text">
+                        <p class="description">Email addresses to receive booking notifications (comma-separated for multiple)</p>
+                    </td>
+                </tr>
+
+                <tr>
+                    <th scope="row">
+                        <label for="rmgc_admin_email_subject">Admin Email Subject</label>
+                    </th>
+                    <td>
+                        <input type="text" id="rmgc_admin_email_subject" name="rmgc_admin_email_subject" value="<?php echo esc_attr($admin_email_subject); ?>" class="regular-text">
+                        <p class="description">Subject line for staff notification emails</p>
+                    </td>
+                </tr>
+
+                <tr>
+                    <th scope="row">
+                        <label for="rmgc_guest_email_subject">Guest Email Subject</label>
+                    </th>
+                    <td>
+                        <input type="text" id="rmgc_guest_email_subject" name="rmgc_guest_email_subject" value="<?php echo esc_attr($guest_email_subject); ?>" class="regular-text">
+                        <p class="description">Subject line for guest confirmation emails</p>
                     </td>
                 </tr>
                 
